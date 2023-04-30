8f9148c66c984ace5f7914112d2e60bf87abc8d8
starslilia <93939810+starslilia@users.noreply.github.com>
2023-04-30 23:16:36 +0000

    lista 2 e 3

---
+++ lista 3/1930.py
@@ -0,0 +1,7 @@
+t = input().split()
+t1 = int(t[0])
+t2 = int(t[1])
+t3 = int(t[2])
+t4 = int(t[3])
+max = (t1 + t2 + t3 + t4) - 3
+print(max)
---
+++ lista 3/2416.py
@@ -0,0 +1,5 @@
+c, n = input().split()
+c1 = int(c)
+n1 = int(n)
+t = c1 % n1
+print(t)
